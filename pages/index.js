01bb925e1baec5811ce8a3667f49021512ddf4e5
CesarRossi <73624132+cesarossi@users.noreply.github.com>
2023-09-01 16:28:17 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-function Home(){
-    return <h1>Oi Le, essa mensagem é pra você, pra que saiba que estou sempre contigo, pro que você precisar.</h1>;
+function Home() {
+  return (
+    <h1>
+      Oi Le, essa mensagem é pra você, pra que saiba que estou sempre contigo,
+      pro que você precisar.
+    </h1>
+  );
 }
-
-
 export default Home;
